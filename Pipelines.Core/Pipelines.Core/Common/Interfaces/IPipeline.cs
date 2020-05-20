c2b7686990c7018a6da0e48e4f6a29b00948c7f7
Muhammad Razali <wireless-@hotmail.com>
2020-05-20 17:27:11 +0800

    Created IPipeline

---
+++ Pipelines.Core/Pipelines.Core/Common/Interfaces/IPipeline.cs
@@ -0,0 +1,34 @@
+﻿namespace Pipelines.Core.Common.Interfaces
+{
+
+    /// <summary>
+    /// Decompose a task that performs complex processing into a series of discrete elements that can be reused. 
+    /// This pattern can improve performance, scalability, and reusability by allowing task elements that 
+    /// perform the processing to be deployed and scaled independently.
+    /// </summary>
+    /// <typeparam name="InOutput"></typeparam>
+    public interface IPipeline<InOutput> : IPipeline<InOutput, InOutput>
+    {
+
+    }
+
+    /// <summary>
+    /// Decompose a task that performs complex processing into a series of discrete elements that can be reused. 
+    /// This pattern can improve performance, scalability, and reusability by allowing task elements that 
+    /// perform the processing to be deployed and scaled independently.
+    /// </summary>
+    /// <typeparam name="Input"></typeparam>
+    /// <typeparam name="Output"></typeparam>
+
+    public interface IPipeline<Input, Output>
+    {
+        IPipeline<Input, Output> Register<FilterIn, FilterOut>(IFilter<FilterIn, FilterOut> filter);
+
+        IPipeline<Input, Output> Register<FilterInOut>(IFilter<FilterInOut> filter);
+
+        IPipeline<Input, Output> CreatePipeline();
+
+        Output Process(Input input);
+
+    }
+}
